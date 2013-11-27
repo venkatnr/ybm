3aad72f7b1136ed8ca6a9636783bba0fcb675796
venkatnr <nishankararamu@gmail.com>
2013-11-28 02:32:41 +0530

    first commit ybm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ybm::Application.config.secret_token = '39c9c29a87d4a183edb3007931a559160347e456fb3ece9b1442ecb5a1162d3da41fea9c9339c90c1bddf07ca88b2580ac0aca644d2ed70cf1b79c73edcda58e'
